1c50e4b6af9e2cbaca2bff26c0ea0a97233f88ff
Adrian Velasquez <a.velasquezs@uniandes.edu.co>
2024-10-17 10:58:58 -0500

    add files

---
+++ dist/Series.js
@@ -0,0 +1,7 @@
+"use strict";
+class Series {
+    constructor(title, episodes) {
+        this.title = title;
+        this.episodes = episodes;
+    }
+}
---
+++ src/Series.ts
@@ -0,0 +1,10 @@
+class Series {
+  title: string;
+  episodes: number;
+
+  constructor(title: string, episodes: number) {
+    this.title = title;
+    this.episodes = episodes;
+
+  }
+}
